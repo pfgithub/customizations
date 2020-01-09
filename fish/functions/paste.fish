8579d1285a8c75311db371039373a60d56365d9d
pfgithub <omegariisthebest@gmail.com>
2020-01-09 10:00:02 -0800

    Update paste.fish

--- fish/functions/paste.fish
+++ fish/functions/paste.fish
@@ -1,4 +1,12 @@
 # Defined in - @ line 1
-function paste --description 'alias paste=xclip -o'
-	xclip -selection clipboard -o $argv;
+function paste --argument filename
+	if type -q pbpaste
+		if test -n "$filename"
+			pbpaste > $filename;
+		else
+			pbpaste
+		end
+	else
+		xclip -selection clipboard -o $argv
+	end
 end

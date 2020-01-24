615fa04e3a1e916a3c1b47b06c98ac518b7af3b9
pfg <omegariisthebest@gmail.com>
2020-01-23 17:23:48 -0800

    Create format.fish

---
+++ fish/functions/format.fish
@@ -0,0 +1,15 @@
+function format
+      for file in $argv
+              echo "Formatting" $file
+              set --local extension (echo $file | awk -F. '{print $NF}')
+              switch $extension
+              case "zig"
+                      zig fmt $file
+              case "cpp" "c" "h" "hpp"
+                      clang-format -i $file
+              case '*'
+                      prettier --write $file
+              end
+              echo "Done"
+      end
+end

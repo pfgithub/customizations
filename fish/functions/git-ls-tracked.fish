fe951f5cbf8cd68cb33222bb47582082d591ec27
pfg <omegariisthebest@gmail.com>
2020-01-23 17:23:06 -0800

    Create git-ls-tracked.fish

---
+++ fish/functions/git-ls-tracked.fish
@@ -0,0 +1,8 @@
+function git-ls-tracked
+        for file in (find . -type f -not -path '*node_modules*' -not -path '*.git*' -not -path './junk*')
+                git check-ignore $file -q
+                if test $status -eq 0; else
+                        echo $file
+                end
+        end
+end

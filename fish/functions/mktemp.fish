68386b6d10b9f22ffa0e32595c2550146b92a801
pfg <omegariisthebest@gmail.com>
2020-04-13 22:45:10 -0700

    Update mktemp.fish

--- fish/functions/mktemp.fish
+++ fish/functions/mktemp.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 function mktemp
-	set tmpdir (openssl rand -base64 12)
+	set tmpdir (openssl rand -hex 12)
 	mkdir ~/Dev/Node/temp/generated/$tmpdir
 	cd ~/Dev/Node/temp/generated/$tmpdir
 end

d5979b0e39ebf4090359186a68b9866d772b0da6
pfg <omegariisthebest@gmail.com>
2020-04-13 22:44:49 -0700

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -24,4 +24,7 @@ function ifconfig
 	node -e "var os=require(\"os\"),ifaces=os.networkInterfaces();Object.keys(ifaces).forEach((e)=>{var o=0;ifaces[e].forEach((s)=>{!1===s.internal&&(console.log(`\${s.family} - \${s.address} - \${o>=1?e+\":\"+o:e}`),++o)})});"
 end
 
-thefuck --alias | source
+if [ (pwd) = ~/Dev/Node/temp/waiting ]
+	mktemp
+end
+

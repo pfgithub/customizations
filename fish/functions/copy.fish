06bc60dcdcd457d021ebbfdc136e916fb66171b5
pfg <pfg@pfg.pw>
2022-11-16 16:58:48 -0500

    support android for copy command

--- fish/functions/copy.fish
+++ fish/functions/copy.fish
@@ -1,8 +1,13 @@
 function copy --argument filename
     if type -q pbcopy
         cat $argv | pbcopy
-    else
+    else if type -q xclip
         xclip -selection clipboard $argv
         xclip -selection primary $argv
+    else if type -q termux-clipboard-set
+        cat $argv | termux-clipboard-set
+    else
+        echo "clipboard not supported"
+        false
     end
 end
